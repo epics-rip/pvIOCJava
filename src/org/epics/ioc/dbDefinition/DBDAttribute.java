044e00cf692a8d72a31d9b1464e2f2b33fadf09f
kraimer <unknown>
2006-05-19 19:21:41 +0000

    Mostly Documentation

--- src/org/epics/ioc/dbDefinition/DBDAttribute.java
+++ src/org/epics/ioc/dbDefinition/DBDAttribute.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
 package org.epics.ioc.dbDefinition;
 import org.epics.ioc.pvAccess.*;
 
