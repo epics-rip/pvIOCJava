daa26175bacb800d145ecfe27dc9e7bd340a69ef
kraimer <unknown>
2007-06-05 16:55:48 +0000

    asyn to pdrv conversion in progress

---
+++ src/org/epics/ioc/v3a/AsynLink.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.v3a;
+
+/**
+ * @author mrk
+ *
+ */
+public class AsynLink {
+    public String portName;
+    public int addr;
+    public int mask;
+    public double timeOut;
+    public String drvParams;
+}
