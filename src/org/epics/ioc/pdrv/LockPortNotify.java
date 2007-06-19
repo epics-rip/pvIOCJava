8c0e97c93e9b7d48ec417af27c2e5d95c10d6264
kraimer <unknown>
2007-06-19 19:16:18 +0000

    interim commit

--- src/org/epics/ioc/pdrv/LockPortNotify.java
+++ src/org/epics/ioc/pdrv/LockPortNotify.java
@@ -7,6 +7,13 @@ package org.epics.ioc.pdrv;
 
 /**
  * Interface for code that wants to be notified when port.lock or port.unlock are called.
+ * This is for use by a driver that is a user of another port. For example a multi-drop serial
+ * driver can be implemented by being the user of a standard serial port.
+ * When a user of the multi-drop driver becomes the port owner, the multi-drop driver can call lockPort
+ * for the serial port and when the user of the multi-drop driver is no longer the port owner,
+ * the multi-drop driver calls unlockPort for the serial port.
+ * It is possible for lock to be called again BEFORE unlock is called.
+ * The implementation must be prepared to handle this race condition.
  * @author mrk
  *
  */
