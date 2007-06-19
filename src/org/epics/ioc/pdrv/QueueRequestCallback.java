f73be0f39330da2292b9783cd51e1b3d94a640ec
kraimer <unknown>
2007-06-19 19:16:20 +0000

    interim commit

--- src/org/epics/ioc/pdrv/QueueRequestCallback.java
+++ src/org/epics/ioc/pdrv/QueueRequestCallback.java
@@ -14,7 +14,11 @@ package org.epics.ioc.pdrv;
 public interface QueueRequestCallback {
     /**
      * The queueRequest has been dequeued.
+     * @param status Status of the request.
+     * The request will fail if the port is not connected or if it is disabled.
+     * If the status is not Status.success than the user must NOT
+     * call and interfaces for the port and user.getMessage provides the reason.
      * @param user The user.
      */
-    void callback(User user);
+    void callback(Status status,User user);
 }
