4303bca4f73f530ff91abd0711de277c682602ae
kraimer <unknown>
2008-09-12 16:01:15 +0000

    vxi11 implemented

---
+++ src/org/epics/ioc/pdrv/vxi11/VXI11ErrorCode.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.pdrv.vxi11;
+
+public enum VXI11ErrorCode {
+    noError,
+    syntaxError,
+    deviceNotAccessable,
+    invalidLinkIdentifier,
+    parameterError,
+    channelNotEstablished,
+    operationNotSupported,
+    outOfResources,
+    deviceLockedByAnotherLink,
+    noLockHeldByThisLink,
+    IOTimeout,
+    IOError,
+    invalidAddress,
+    abort,
+    channelAlreadyEstablished,
+    unknownHostException,
+    IOException,
+    RPCException,
+    unknown;
+}
