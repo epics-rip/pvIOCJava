2c6fa3e1c3b1b17a71dcf31178630b7b0d70a945
mrkraimer <unknown>
2009-03-20 16:49:20 +0000

    fix some bugs. add method getSupports

--- src/org/epics/ioc/support/basic/GenericBase.java
+++ src/org/epics/ioc/support/basic/GenericBase.java
@@ -11,7 +11,9 @@ import org.epics.ioc.support.Support;
 import org.epics.ioc.support.SupportProcessRequester;
 import org.epics.ioc.support.SupportState;
 import org.epics.ioc.support.alarm.AlarmSupport;
+import org.epics.ioc.support.alarm.AlarmSupportFactory;
 import org.epics.ioc.util.RequestResult;
+import org.epics.pvData.pv.MessageType;
 import org.epics.pvData.pv.PVBoolean;
 import org.epics.pvData.pv.PVField;
 import org.epics.pvData.pv.PVStructure;
@@ -42,6 +44,13 @@ public class GenericBase extends AbstractSupport implements SupportProcessReques
         this.pvStructure = pvStructure;
         processRequesterName = pvStructure.getFullName();
     }
+    /**
+     * Get the supports for the fields in this structure.
+     * @return
+     */
+    protected Support[] getSupports() {
+        return supports;
+    }
     /* (non-Javadoc)
      * @see org.epics.ioc.process.SupportProcessRequester#getProcessRequesterName()
      */
@@ -65,19 +74,14 @@ public class GenericBase extends AbstractSupport implements SupportProcessReques
             if(fieldName.equals("timeStamp")) continue;
             // alarm is a special case
             if(fieldName.equals("alarm")) {
-                Support support = recordSupport.getSupport(pvField);
-                if(support!=null && support instanceof AlarmSupport) {
-                    alarmSupport = (AlarmSupport)support;
+                alarmSupport = AlarmSupportFactory.findAlarmSupport(pvStructure,recordSupport);
+                if(alarmSupport==null) {
+                    super.getPVField().message("illegal alarm field", MessageType.error);
+                    return;
                 }
                 continue;
             }
             if(recordSupport.getSupport(pvFields[i])!=null) numberSupport++;
-        }
-        if(alarmSupport!=null) {
-            alarmSupport.initialize(recordSupport);
-            if(alarmSupport.getSupportState()!=SupportState.readyForStart) {
-                return;
-            }
         }
         pvWaits = new PVBoolean[numberSupport];
         supports = new Support[numberSupport];
@@ -99,12 +103,19 @@ public class GenericBase extends AbstractSupport implements SupportProcessReques
             support.initialize(recordSupport);
             if(support.getSupportState()!=SupportState.readyForStart) {
                 supportState = SupportState.readyForInitialize;
-                for(int j=0; j<indSupport; j++) {
-                    if(supports[j]!=null) supports[j].uninitialize();
+                for(int j=0; j<indSupport-1; j++) {
+                    supports[j].uninitialize();
                 }
-                break;
+                return;
             }
             indSupport++;
+        }
+        if(alarmSupport!=null) {
+            alarmSupport.initialize(recordSupport);
+            if(alarmSupport.getSupportState()!=SupportState.readyForStart) {
+                for(Support support : supports) support.uninitialize();
+                return;
+            }
         }
         setSupportState(supportState);
     }
@@ -117,7 +128,17 @@ public class GenericBase extends AbstractSupport implements SupportProcessReques
             alarmSupport.start();
             if(alarmSupport.getSupportState()!=SupportState.ready) return;
         }
-        for(Support support : supports) support.start();
+        for(int i=0; i<supports.length; i++) {
+            Support support = supports[i];
+            support.start();
+            if(support.getSupportState()!=SupportState.ready) {
+                for(int j=0; j<i; j++) {
+                    supports[j].stop();
+                }
+                alarmSupport.stop();
+                return;
+            }
+        }
         setSupportState(SupportState.ready);
     }
     /* (non-Javadoc)
