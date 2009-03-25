ddc2668d419965c68323331f929bd9978a3b7820
mrkraimer <unknown>
2009-03-25 20:17:26 +0000

    lots of work on portDriver

---
+++ src/org/epics/ioc/support/pdrv/AbstractPortDriverInterruptLink.java
@@ -0,0 +1,190 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.support.pdrv;
+
+
+import org.epics.ioc.pdrv.Device;
+import org.epics.ioc.pdrv.Factory;
+import org.epics.ioc.pdrv.Port;
+import org.epics.ioc.pdrv.QueueRequestCallback;
+import org.epics.ioc.pdrv.Status;
+import org.epics.ioc.pdrv.Trace;
+import org.epics.ioc.pdrv.User;
+import org.epics.ioc.support.AbstractSupport;
+import org.epics.ioc.support.RecordProcess;
+import org.epics.ioc.support.RecordProcessRequester;
+import org.epics.ioc.support.RecordSupport;
+import org.epics.ioc.support.SupportProcessRequester;
+import org.epics.ioc.support.SupportState;
+import org.epics.ioc.support.alarm.AlarmSupport;
+import org.epics.ioc.support.alarm.AlarmSupportFactory;
+import org.epics.ioc.util.RequestResult;
+import org.epics.pvData.factory.ConvertFactory;
+import org.epics.pvData.property.PVProperty;
+import org.epics.pvData.property.PVPropertyFactory;
+import org.epics.pvData.pv.Convert;
+import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.PVBoolean;
+import org.epics.pvData.pv.PVField;
+import org.epics.pvData.pv.PVRecord;
+import org.epics.pvData.pv.PVString;
+import org.epics.pvData.pv.PVStructure;
+
+/**
+ * Abstract  base class for PortDriverInterruptLink.
+ * It creates a User, connects to a port and a device, and attempts to become the recordProcessor
+ * if process is true. Then derived class must handle the interrupt.
+ * @author mrk
+ *
+ */
+public abstract class AbstractPortDriverInterruptLink extends AbstractSupport
+implements RecordProcessRequester,QueueRequestCallback,PortDriverInterruptLink
+{
+    /**
+     * Constructor for derived PortDriverInterruptLink Support.
+     * @param supportName The support name.
+     * @param dbStructure The link interface.
+     */
+    protected AbstractPortDriverInterruptLink(String supportName,PVStructure pvStructure) {
+        super(supportName,pvStructure);
+        this.supportName = supportName;
+        this.pvStructure = pvStructure;
+        fullName = pvStructure.getFullName();
+        pvRecord = pvStructure.getPVRecord();
+        recordName = pvRecord.getRecordName();
+    }  
+    
+    protected static Convert convert = ConvertFactory.getConvert();
+    protected static PVProperty pvProperty = PVPropertyFactory.getPVProperty(); 
+    protected String supportName;
+    protected PVStructure pvStructure;
+    protected String fullName = null;
+    protected PVRecord pvRecord = null;
+    protected String recordName = null;
+    
+    protected RecordProcess recordProcess = null;
+    protected PVField valuePVField = null;
+    protected AlarmSupport alarmSupport = null;
+    protected PVString pvPortName = null;
+    protected PVString pvDeviceName = null;
+    protected PVBoolean pvProcess = null;
+    
+    protected User user = null;
+    protected Port port = null;
+    protected Trace portTrace = null;
+    protected Device device = null;
+    protected Trace deviceTrace = null;
+    private boolean isProcessor = false;
+    
+    protected SupportProcessRequester supportProcessRequester = null;
+    
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.AbstractSupport#initialize(org.epics.ioc.support.RecordSupport)
+     */
+    public void initialize(RecordSupport recordSupport) {
+        if(!super.checkSupportState(SupportState.readyForInitialize,supportName)) return;
+        recordProcess = recordSupport.getRecordProcess();
+        PVStructure pvParent = pvStructure.getParent();
+        while(pvParent!=null) {
+            valuePVField = pvParent.getSubField("value");
+            if(valuePVField!=null) break;
+            pvParent = pvParent.getParent();
+        }
+        if(valuePVField==null) {
+            pvStructure.message("value field not found", MessageType.error);
+            return;
+        }
+        alarmSupport = AlarmSupportFactory.findAlarmSupport(pvStructure,recordSupport);
+        pvPortName = pvStructure.getStringField("portName");
+        if(pvPortName==null) return;
+        pvDeviceName = pvStructure.getStringField("deviceName");
+        if(pvDeviceName==null) return;
+        pvProcess = pvStructure.getBooleanField("process");
+        setSupportState(SupportState.readyForStart);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.AbstractSupport#uninitialize()
+     */
+    public void uninitialize() {
+        if(super.getSupportState()==SupportState.ready) {
+            stop();
+        }
+        if(super.getSupportState()!=SupportState.readyForStart) return;
+        pvProcess = null;
+        pvDeviceName = null;
+        pvPortName = null;
+        alarmSupport = null;
+        valuePVField = null;
+        recordProcess = null;
+        setSupportState(SupportState.readyForInitialize);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.AbstractSupport#start()
+     */
+    public void start() {
+        if(!super.checkSupportState(SupportState.readyForStart,supportName)) return;
+        user = Factory.createUser(this);
+        port = user.connectPort(pvPortName.get());
+        if(port==null) {
+            pvStructure.message(user.getMessage(),MessageType.error);
+            return;
+        }
+        portTrace = port.getTrace();
+        device = user.connectDevice(pvDeviceName.get());
+        if(device==null) {
+            pvStructure.message(user.getMessage(),MessageType.error);
+            return;
+        }
+        deviceTrace = device.getTrace();
+        if(pvProcess!=null && pvProcess.get()) {
+            isProcessor =recordProcess.setRecordProcessRequester(this);
+            if(!isProcessor) {
+                pvStructure.message("could not become record processor", MessageType.error);
+            }
+        }
+        setSupportState(SupportState.ready);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.AbstractSupport#stop()
+     */
+    public void stop() {
+        if(super.getSupportState()!=SupportState.ready) return;
+        if(isProcessor) recordProcess.releaseRecordProcessRequester(this);
+        isProcessor = false;
+        user.disconnectPort();
+        device = null;
+        deviceTrace = null;
+        port = null;
+        portTrace = null;
+        user = null;
+        setSupportState(SupportState.readyForStart);
+    } 
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.pdrv.PDRVInterruptSupport#isProcess()
+     */
+    public boolean isProcess() {
+        return isProcessor;
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.pdrv.QueueRequestCallback#callback(org.epics.ioc.pdrv.Status, org.epics.ioc.pdrv.User)
+     */
+    public void callback(Status status, User user) {
+        deviceTrace.print(Trace.ERROR,
+                "%s:%s why was callback called???",
+                fullName,supportName);
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.RecordProcessRequester#recordProcessComplete()
+     */
+    public void recordProcessComplete() {}
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.RecordProcessRequester#recordProcessResult(org.epics.ioc.util.RequestResult)
+     */
+    public void recordProcessResult(RequestResult requestResult) {}
+}
---
+++ src/org/epics/ioc/support/pdrv/AbstractPortDriverSupport.java
@@ -0,0 +1,204 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.support.pdrv;
+
+
+import org.epics.ioc.pdrv.Device;
+import org.epics.ioc.pdrv.Port;
+import org.epics.ioc.pdrv.Trace;
+import org.epics.ioc.pdrv.User;
+import org.epics.ioc.support.RecordSupport;
+import org.epics.ioc.support.Support;
+import org.epics.ioc.support.SupportProcessRequester;
+import org.epics.ioc.support.SupportState;
+import org.epics.ioc.support.alarm.AlarmSupport;
+import org.epics.ioc.support.basic.GenericBase;
+import org.epics.pvData.factory.ConvertFactory;
+import org.epics.pvData.property.PVProperty;
+import org.epics.pvData.property.PVPropertyFactory;
+import org.epics.pvData.pv.Convert;
+import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.PVField;
+import org.epics.pvData.pv.PVRecord;
+import org.epics.pvData.pv.PVStructure;
+
+/**
+ * Abstract link support base class for PDRV links.
+ * It handles all the generic things for a PDRV link.
+ * It connects locates the interfaces for all the PDRV fields in the link structure.
+ * For each of the lifetime methods it provides methods (initBase, startBase, stopBase, uninitBase)
+ * than must be called by derived classes.
+ * The process and message implementation should be sufficient for most derived support.
+ * @author mrk
+ *
+ */
+public abstract class AbstractPortDriverSupport extends GenericBase
+implements PortDriverSupport
+{
+    /**
+     * Constructor for derived support.
+     * @param supportName The support name.
+     * @param dbStructure The link interface.
+     */
+    protected AbstractPortDriverSupport(String supportName,PVStructure pvStructure) {
+        super(supportName,pvStructure);
+        this.supportName = supportName;
+        this.pvStructure = pvStructure;
+        fullName = pvStructure.getFullName();
+        pvRecord = pvStructure.getPVRecord();
+        recordName = pvRecord.getRecordName();
+    }
+   
+    protected static Convert convert = ConvertFactory.getConvert();
+    protected static PVProperty pvProperty = PVPropertyFactory.getPVProperty(); 
+    protected static final String emptyString = "";
+    protected String supportName;
+    protected PVStructure pvStructure;
+    protected PVRecord pvRecord = null;
+    protected String recordName = null;
+    protected String fullName = null;
+    
+    protected PVField valuePVField = null;
+    protected AlarmSupport alarmSupport = null;
+   
+    protected PortDriverLink portDriverLink = null;
+    protected User user = null;
+    protected Port port = null;
+    protected Device device = null;
+    protected Trace portTrace = null;
+    protected Trace deviceTrace = null;
+    
+    protected SupportProcessRequester supportProcessRequester = null;
+    private PortDriverSupport[] portDriverSupports = null;
+    
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.basic.GenericBase#initialize(org.epics.ioc.support.RecordSupport)
+     */
+    public void initialize(RecordSupport recordSupport) {
+        super.initialize(recordSupport);
+        if(!super.checkSupportState(SupportState.readyForStart,supportName)) return;
+        PVStructure pvParent = pvStructure.getParent();
+        while(pvParent!=null) {
+            valuePVField = pvParent.getSubField("value");
+            if(valuePVField!=null) break;
+            pvParent = pvParent.getParent();
+        }
+        if(valuePVField==null) {
+            super.uninitialize();
+            return;
+        }
+        Support[] supports = super.getSupports();
+        int number = 0;
+        for(Support support : supports) {
+            if(support instanceof PortDriverSupport) number++;
+        }
+        portDriverSupports = new PortDriverSupport[number];
+        int index = 0;
+        for(Support support : supports) {
+            if(support instanceof PortDriverSupport) {
+                portDriverSupports[index++] = (PortDriverSupport)support;
+            }
+        }
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.basic.GenericBase#uninitialize()
+     */
+    public void uninitialize() {
+        super.uninitialize();
+        alarmSupport = null;
+        valuePVField = null;
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.basic.GenericBase#start()
+     */
+    public void start() {
+        if(!super.checkSupportState(SupportState.readyForStart,supportName)) return;
+        port = user.getPort();
+        if(port==null) {
+            pvStructure.message(user.getMessage(),MessageType.error);
+            return;
+        }
+        portTrace = port.getTrace();
+        device = user.getDevice();
+        if(device==null) {
+            pvStructure.message(user.getMessage(),MessageType.error);
+            return;
+        }
+        deviceTrace = device.getTrace();
+        for(PortDriverSupport portDriverSupport : portDriverSupports) {
+            portDriverSupport.setPortDriverLink(portDriverLink);
+        }
+        super.start();
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.pdrv.PortDriverSupport#setPortDriverLink(org.epics.ioc.support.pdrv.PortDriverLink)
+     */
+    @Override
+    public void setPortDriverLink(PortDriverLink portDriverLink) {
+        this.portDriverLink = portDriverLink;
+        user = portDriverLink.getUser();
+        alarmSupport = portDriverLink.getAlarmSupport();
+        for(PortDriverSupport portDriverSupport : portDriverSupports) {
+            portDriverSupport.setPortDriverLink(portDriverLink);
+        }
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.basic.GenericBase#stop()
+     */
+    public void stop() {
+        super.stop();
+        device = null;
+        deviceTrace = null;
+        port = null;
+        portTrace = null;
+        user = null;
+    }     
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.basic.GenericBase#process(org.epics.ioc.support.SupportProcessRequester)
+     */
+    @Override
+    public void process(SupportProcessRequester supportProcessRequester) {
+        throw new IllegalStateException("process shold never be called");
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.pdrv.PortDriverSupport#processCallback()
+     */
+    @Override
+    public void beginProcess() {
+        for(PortDriverSupport portDriverSupport : portDriverSupports) {
+            portDriverSupport.beginProcess();
+        }
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.pdrv.PortDriverSupport#endProcess()
+     */
+    @Override
+    public void endProcess() {
+        for(PortDriverSupport portDriverSupport : portDriverSupports) {
+            portDriverSupport.endProcess();
+        }
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.pdrv.PortDriverSupport#queueCallback()
+     */
+    @Override
+    public void queueCallback() {
+        for(PortDriverSupport portDriverSupport : portDriverSupports) {
+            portDriverSupport.queueCallback();
+        }
+    }
+    /* (non-Javadoc)
+     * @see org.epics.ioc.support.AbstractSupport#message(java.lang.String, org.epics.ioc.util.MessageType)
+     */
+    public void message(String message,MessageType messageType) {
+        pvRecord.lock();
+        try {
+            pvStructure.message(message, messageType);
+        } finally {
+            pvRecord.unlock();
+        }
+    }
+}
