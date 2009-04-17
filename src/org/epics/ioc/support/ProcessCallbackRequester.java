3b92fde3dc8c3780d18da5b632a212bac3099c05
mrkraimer <unknown>
2009-04-17 19:35:18 +0000

    changes because of new install facility

--- src/org/epics/ioc/support/ProcessCallbackRequester.java
+++ src/org/epics/ioc/support/ProcessCallbackRequester.java
@@ -11,11 +11,11 @@ import org.epics.pvData.pv.Requester;
  * <p>The interface that must be implemented by code that makes a recordProcess.requestProcessCallback
  * request, which results in method processCallback being called.
  * processCallback is called with the record unlocked.
- * It is called just before recordSupport.process or recordSupport.processContinue returns.
+ * It is called just before locateSupport.process or locateSupport.processContinue returns.
  * </p>
  * <p>Code that wants to process other records can only request processing of the other records via this method.
  * It is NOT permissible to make direct process requests because of "deadly embrace" race conditions,
- * because RecordProcess locks a record instance before calling recordSupport.process.</p>
+ * because RecordProcess locks a record instance before calling locateSupport.process.</p>
  * <p>Code that implements asynchronous support must implement this method.
  * The method itself must not block so it must use another thread to implement asynchronous operations.</p>
  * @author mrk
