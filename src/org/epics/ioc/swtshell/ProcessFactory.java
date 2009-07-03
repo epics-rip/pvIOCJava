fe2dce239aff14fd023ef69de98fe819d0cfc459
mrkraimer <unknown>
2009-07-03 15:47:28 +0000

    process and get have been changed to use new channel access interfaces

--- src/org/epics/ioc/swtshell/ProcessFactory.java
+++ src/org/epics/ioc/swtshell/ProcessFactory.java
@@ -12,20 +12,18 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.epics.ioc.ca.Channel;
-import org.epics.ioc.ca.ChannelListener;
-import org.epics.ioc.ca.ChannelProcess;
-import org.epics.ioc.ca.ChannelProcessRequester;
-import org.epics.ioc.util.RequestResult;
+import org.epics.pvData.channelAccess.Channel;
+import org.epics.pvData.channelAccess.ChannelProcess;
+import org.epics.pvData.channelAccess.ChannelProcessRequester;
+import org.epics.pvData.channelAccess.ChannelRequester;
 import org.epics.pvData.misc.Executor;
-import org.epics.pvData.misc.ExecutorFactory;
 import org.epics.pvData.misc.ExecutorNode;
-import org.epics.pvData.misc.ThreadPriority;
 import org.epics.pvData.pv.MessageType;
 import org.epics.pvData.pv.Requester;
 
@@ -45,74 +43,45 @@ public class ProcessFactory {
         processImpl.start();
     }
 
-    private static class ProcessImpl implements DisposeListener,Requester,ChannelListener,SelectionListener  {
+    private static Executor executor = SwtshellFactory.getExecutor();
+    
+    private static class ProcessImpl implements DisposeListener,ChannelRequester,SelectionListener,Runnable  {
 
         private ProcessImpl(Display display) {
             this.display = display;
         }
 
         private boolean isDisposed = false;
-        private static Executor executor
-            = ExecutorFactory.create("swtshell:Get",ThreadPriority.low);
         private static String windowName = "process";
+        private ExecutorNode executorNode = executor.createNode(this);
         private Display display;
         private Shell shell = null;
         private Requester requester = null;
         private Channel channel = null;
-        private ChannelConnect channelConnect = null;
-        private Button processButton;
+        private Button connectButton = null;
+        private Button disconnectButton = null;
+        private Button processButton = null;
         private Text consoleText = null; 
+        private Process process = null;
 
-        /* (non-Javadoc)
-         * @see org.eclipse.swt.events.DisposeListener#widgetDisposed(org.eclipse.swt.events.DisposeEvent)
-         */
-        public void widgetDisposed(DisposeEvent e) {
-            isDisposed = true;
-            if(channel!=null) channel.destroy();
-        }
-        /* (non-Javadoc)
-         * @see org.epics.ioc.util.Requester#getRequesterName()
-         */
-        public String getRequesterName() {
-            return requester.getRequesterName();
-        }
-        /* (non-Javadoc)
-         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
-         */
-        public void message(String message, MessageType messageType) {
-            requester.message(message, messageType);
-        }
-        /* (non-Javadoc)
-         * @see org.epics.ioc.ca.ChannelListener#channelStateChange(org.epics.ioc.ca.Channel, boolean)
-         */
-        public void channelStateChange(Channel c, boolean isConnected) {
-            if(isDisposed) return;
-            if(isConnected) {
-                channel = channelConnect.getChannel();
-                processButton.setEnabled(true);
-                return;
-            } else {
-                channel = null;
-                processButton.setEnabled(false);
-                return;
-            }
-        }
-        /* (non-Javadoc)
-         * @see org.epics.ioc.ca.ChannelListener#disconnect(org.epics.ioc.ca.Channel)
-         */
-        public void destroy(Channel c) {
-            channelStateChange(c,false);
-        }
-        
         private void start() {
             shell = new Shell(display);
             shell.setText(windowName);
             GridLayout gridLayout = new GridLayout();
             gridLayout.numColumns = 1;
             shell.setLayout(gridLayout);
-            channelConnect = ChannelConnectFactory.create(this,this);
-            channelConnect.createWidgets(shell,false,false);
-            processButton = new Button(shell,SWT.PUSH);
+            Composite composite = new Composite(shell,SWT.BORDER);
+            RowLayout rowLayout = new RowLayout(SWT.HORIZONTAL);
+            composite.setLayout(rowLayout);
+            connectButton = new Button(composite,SWT.PUSH);
+            connectButton.setText("connect");
+            connectButton.addSelectionListener(this);               
+            connectButton.setEnabled(true);
+            disconnectButton = new Button(composite,SWT.PUSH);
+            disconnectButton.setText("disconnect");
+            disconnectButton.addSelectionListener(this);               
+            disconnectButton.setEnabled(false);
+            processButton = new Button(composite,SWT.PUSH);
             processButton.setText("process");
             processButton.addSelectionListener(this);               
             processButton.setEnabled(false);
@@ -145,61 +114,178 @@ public class ProcessFactory {
             shell.addDisposeListener(this);
         }
         /* (non-Javadoc)
+         * @see org.eclipse.swt.events.DisposeListener#widgetDisposed(org.eclipse.swt.events.DisposeEvent)
+         */
+        @Override
+        public void widgetDisposed(DisposeEvent e) {
+            isDisposed = true;
+            executor.execute(executorNode);
+            
+        }
+        /* (non-Javadoc)
          * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
          */
+        @Override
         public void widgetDefaultSelected(SelectionEvent arg0) {
             widgetSelected(arg0);
         }
         /* (non-Javadoc)
          * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
          */
+        @Override
         public void widgetSelected(SelectionEvent arg0) {
             if(isDisposed) return;
-            Object object = arg0.getSource(); 
-            if(object==processButton) {
-                new ProcessIt(channel,this);
+            Object object = arg0.getSource();
+            if(object==connectButton) {
+                ConnectChannel connectChannel = ConnectChannelFactory.create(shell, this);
+                connectChannel.connect();
+            } else if(object==disconnectButton) {
+                process.disconnect();
+                connectButton.setEnabled(true);
+                disconnectButton.setEnabled(false);
+                processButton.setEnabled(false);
+                channel = null;
+            } else if(object==processButton) {
+                process.process();
                 return;
             }
         }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#getRequesterName()
+         */
+        @Override
+        public String getRequesterName() {
+            return requester.getRequesterName();
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+         */
+        @Override
+        public void message(String message, MessageType messageType) {
+            requester.message(message, messageType);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.pvData.channelAccess.ChannelRequester#channelCreated(org.epics.pvData.channelAccess.Channel)
+         */
+        @Override
+        public void channelCreated(Channel channel) {
+            this.channel = channel;
+            message("channel created",MessageType.info);
+            display.asyncExec(this);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.pvData.channelAccess.ChannelRequester#channelNotCreated()
+         */
+        @Override
+        public void channelNotCreated() {
+            message("channel not created",MessageType.error);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.ca.ChannelRequester#channelStateChange(org.epics.ioc.ca.Channel, boolean)
+         */
+        @Override
+        public void channelStateChange(Channel c, boolean isConnected) {
+            display.asyncExec(this);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.ca.ChannelRequester#disconnect(org.epics.ioc.ca.Channel)
+         */
+        @Override
+        public void destroy(Channel c) {
+            display.asyncExec(this);
+        }
         
-        private class ProcessIt implements Runnable,ChannelProcessRequester
+        /* (non-Javadoc)
+         * @see java.lang.Runnable#run()
+         */
+        @Override
+        public void run() {
+            if(isDisposed) {
+                if(channel!=null) channel.destroy();
+                return;
+            }
+            if(channel==null) return;
+            boolean isConnected = channel.isConnected();
+            if(isConnected) {
+                connectButton.setEnabled(false);
+                disconnectButton.setEnabled(true);
+                process = new Process(channel,this);
+                processButton.setEnabled(true);
+            } else {
+                connectButton.setEnabled(true);
+                disconnectButton.setEnabled(false);
+                processButton.setEnabled(false);
+            }
+        }
+        private enum RunCommand{process,disconnect};
+        
+        private static class Process implements Runnable,ChannelProcessRequester
         {   
-            final private Requester requester;
+            private Channel channel;
+            private Requester requester;
             private ChannelProcess channelProcess;
             private ExecutorNode executorNode;
+            
+            private RunCommand runCommand = null;
 
-            private ProcessIt(Channel channel,Requester requester) {
+            private Process(Channel channel,Requester requester) {
+                this.channel = channel;
                 this.requester = requester;
                 executorNode = executor.createNode(this);
-                channelProcess = channel.createChannelProcess(this);
-                if(channelProcess!=null) executor.execute(executorNode);
+                channel.createChannelProcess(this);
+            }
+            
+            private void process() {
+                runCommand = RunCommand.process;
+                executor.execute(executorNode);
+            }
+            
+            private void disconnect() {
+                runCommand = RunCommand.disconnect;
+                executor.execute(executorNode);
+            }
+            /* (non-Javadoc)
+             * @see org.epics.pvData.channelAccess.ChannelProcessRequester#channelProcessConnect(org.epics.pvData.channelAccess.ChannelProcess)
+             */
+            @Override
+            public void channelProcessConnect(ChannelProcess channelProcess) {
+                this.channelProcess = channelProcess;
+            }
+            /* (non-Javadoc)
+             * @see org.epics.pvData.channelAccess.ChannelProcessRequester#processDone(boolean)
+             */
+            @Override
+            public void processDone(boolean success) {
+                message("processDone success " + success,MessageType.info);
             }
             /* (non-Javadoc)
              * @see java.lang.Runnable#run()
              */
+            @Override
             public void run() {
-                channelProcess.process();
-               
+                switch(runCommand) {
+                case process:
+                    channelProcess.process(false);
+                    break;
+                case disconnect:
+                    channelProcess.destroy();
+                    channel.destroy();
+                    break;
+                }
             }
             /* (non-Javadoc)
              * @see org.epics.ioc.util.Requester#getRequesterName()
              */
+            @Override
             public String getRequesterName() {
                 return requester.getRequesterName();
             }
             /* (non-Javadoc)
              * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
              */
+            @Override
             public void message(final String message, final MessageType messageType) {
                 requester.message(message, MessageType.info);
-            }
-            /* (non-Javadoc)
-             * @see org.epics.ioc.ca.ChannelProcessRequester#processDone(org.epics.ioc.util.RequestResult)
-             */
-            public void processDone(RequestResult requestResult) {
-                channelProcess.destroy();
-                requester.message("processComplete", MessageType.info);
-                
             }
         }
     }
