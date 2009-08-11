c9ed1c52634e4a1cefc3e5fd1b404581a98e4dec
mrkraimer <unknown>
2009-08-11 10:59:59 +0000

    change init to start add AfterStart so that caLinks have time to connect

--- src/org/epics/ioc/caV4/ClientFactory.java
+++ src/org/epics/ioc/caV4/ClientFactory.java
@@ -14,6 +14,9 @@
 
 package org.epics.ioc.caV4;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.epics.ca.CAConstants;
 import org.epics.ca.CAException;
 import org.epics.ca.client.Channel;
@@ -21,31 +24,91 @@ import org.epics.ca.client.ConnectionEvent;
 import org.epics.ca.client.EventListener;
 import org.epics.ca.core.impl.client.ClientContextImpl;
 import org.epics.ioc.channelAccess.ChannelAccessFactory;
+import org.epics.ioc.install.AfterStart;
+import org.epics.ioc.install.AfterStartFactory;
+import org.epics.ioc.install.AfterStartNode;
+import org.epics.ioc.install.AfterStartRequester;
+import org.epics.ioc.install.NewAfterStartRequester;
 import org.epics.pvData.channelAccess.ChannelFind;
 import org.epics.pvData.channelAccess.ChannelFindRequester;
 import org.epics.pvData.channelAccess.ChannelProvider;
 import org.epics.pvData.channelAccess.ChannelRequester;
+import org.epics.pvData.misc.ThreadPriority;
 
 public class ClientFactory {
+    static private boolean isRegistered = false; 
+    /**
+     * This initializes the Channel Access client.
+     */
+    public static void start() {
+        if(isRegistered) return;
+        isRegistered = true;
+        AfterStartDelay afterStartDelay = new AfterStartDelay();
+        afterStartDelay.start();
+        try {
+            ChannelAccessFactory.registerChannelProvider(new ChannelProviderImpl());
+        } catch (CAException e) {
+            throw new RuntimeException("Failed to initializa client channel access.", e);
+        }
+    }
+    
+    // afterStartDelay ensures that no run method gets called until after 2 seconds after
+    // the last record has started. This allows time to connect to servers.
+    private static class AfterStartDelay extends TimerTask  implements NewAfterStartRequester,AfterStartRequester {
+        private static final Timer timer = new Timer("caClientDelay");
+        private AfterStartNode afterStartNode = null;
+        private AfterStart afterStart = null;
+
+        private AfterStartDelay() {}
+
+        private void start() {
+            afterStartNode = AfterStartFactory.allocNode(this);
+            AfterStartFactory.newAfterStartRegister(this);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.install.NewAfterStartRequester#callback(org.epics.ioc.install.AfterStart)
+         */
+        public void callback(AfterStart afterStart) {
+            this.afterStart = afterStart;
+            afterStart.requestCallback(afterStartNode, false, ThreadPriority.middle);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.install.AfterStartRequester#callback(org.epics.ioc.install.AfterStartNode)
+         */
+        public void callback(AfterStartNode node) {
+            timer.schedule(this, 2000);
+        }
+        /* (non-Javadoc)
+         * @see java.util.TimerTask#run()
+         */
+        @Override
+        public void run() {
+            afterStart.done(afterStartNode);
+            afterStart = null;
+        }
+
+    }
 	
 	private static class ChannelProviderImpl implements ChannelProvider
 	{
-		final ClientContextImpl context;
+	    static private final String providerName = "caV4";
+		private final ClientContextImpl context;
+		
 		
 		public ChannelProviderImpl() throws CAException
 		{
 			context = new ClientContextImpl();
 			context.initialize();
-		}
-
+		}		
 		@Override
 		public ChannelFind channelFind(String channelName,
-				ChannelFindRequester channelFindRequester) {
+				ChannelFindRequester channelFindRequester)
+		{
 			// TODO there is no actual support for find right now in CA... can be done via create
 			throw new RuntimeException("not implemented");
 		}
-
-		@Override
+		
+        @Override
 		public void createChannel(String channelName,
 				final ChannelRequester channelRequester) {
 
@@ -67,28 +130,14 @@ public class ClientFactory {
 			}
 		    channelRequester.channelCreated(channel);
 		}
-
 		@Override
 		public void destroy() {
 			context.dispose();
 		}
-
 		@Override
 		public String getProviderName() {
-			// TODO constant
-			return "CAv4";
+			return providerName;
 		}
 		
 	}
-	
-    /**
-     * This initializes the Channel Access client.
-     */
-    public static void init() {
-    	try {
-			ChannelAccessFactory.registerChannelProvider(new ChannelProviderImpl());
-		} catch (CAException e) {
-			throw new RuntimeException("Failed to initializa client channel access.", e);
-		}
-    }
 }
