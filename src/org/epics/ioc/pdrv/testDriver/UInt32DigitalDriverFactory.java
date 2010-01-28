6ebbe251210b656270a5ba90393db246b89bb996
mrkraimer <unknown>
2010-01-28 11:44:58 +0000

    Instead of the default that only a single object can be the RecordProcessRequester make the default the any object can be it.
    ProcessSelf is gone.

--- src/org/epics/ioc/pdrv/testDriver/UInt32DigitalDriverFactory.java
+++ src/org/epics/ioc/pdrv/testDriver/UInt32DigitalDriverFactory.java
@@ -7,12 +7,12 @@ package org.epics.ioc.pdrv.testDriver;
 
 import org.epics.ioc.pdrv.Device;
 import org.epics.ioc.pdrv.DeviceDriver;
+import org.epics.ioc.pdrv.Factory;
 import org.epics.ioc.pdrv.Port;
 import org.epics.ioc.pdrv.PortDriver;
 import org.epics.ioc.pdrv.Status;
 import org.epics.ioc.pdrv.Trace;
 import org.epics.ioc.pdrv.User;
-import org.epics.ioc.pdrv.Factory;
 import org.epics.ioc.pdrv.interfaces.AbstractUInt32Digital;
 import org.epics.pvData.misc.ThreadPriority;
 import org.epics.pvData.pv.PVDouble;
