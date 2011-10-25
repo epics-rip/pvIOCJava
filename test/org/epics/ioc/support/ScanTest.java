4260583e5f608fa9ebd20b68424bfeb7374e5357
Marty Kraimer <mrkraimer@comcast.net>
2011-10-25 09:42:53 -0400

    start generation regression tests for use by maven. Still work to do.

---
+++ test/org/epics/ioc/support/ScanTest.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this disctibution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.support;
+
+import junit.framework.TestCase;
+
+import org.epics.ioc.database.PVDatabase;
+import org.epics.ioc.database.PVDatabaseFactory;
+import org.epics.ioc.database.PVRecord;
+import org.epics.ioc.install.Install;
+import org.epics.ioc.install.InstallFactory;
+import org.epics.ioc.util.EventScanner;
+import org.epics.ioc.util.PeriodicScanner;
+import org.epics.ioc.util.ScannerFactory;
+import org.epics.ioc.xml.XMLToPVDatabaseFactory;
+import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.PVField;
+import org.epics.pvData.pv.PVStructure;
+import org.epics.pvData.pv.Requester;
+import org.epics.pvData.pv.Structure;
+
+
+/**
+ * JUnit test for scan test.
+ * @author mrk
+ *
+ */
+public class ScanTest extends TestCase {
+    private static final PVDatabase masterPVDatabase = PVDatabaseFactory.getMaster();
+    private static final Install install = InstallFactory.get();
+    private static MessageType maxMessageType = MessageType.info;
+    /**
+     * test scan.
+     */
+    public static void testScan() {
+        PVRecord[] pvRecords = null;
+        Requester iocRequester = new RequesterForTesting("scanTest");
+        XMLToPVDatabaseFactory.convert(masterPVDatabase,"${JAVAIOC}/xml/structures.xml", iocRequester,false,null,null,null);
+        if(maxMessageType!=MessageType.info&&maxMessageType!=MessageType.warning) return;
+        boolean ok = install.installRecords("test/org/epics/ioc/support/scanPV.xml", iocRequester);
+        if(!ok) {
+            System.out.printf("\nrecords\n");
+            pvRecords = masterPVDatabase.getRecords();
+            for(PVRecord pvRecord: pvRecords) {
+                System.out.print(pvRecord.toString());
+            }
+            return;
+        }
+        try {
+            Thread.sleep(1000);
+            System.out.println();
+        } catch (InterruptedException e) {}
+        
+//        System.out.printf("\nrecords\n");
+//        pvRecords = masterPVDatabase.getRecords();
+//        for(PVRecord pvRecord: pvRecords) {
+//            System.out.println(pvRecord.toString());
+//        }
+        
+        PeriodicScanner periodicScanner = ScannerFactory.getPeriodicScanner();
+        String list = periodicScanner.toString();
+        System.out.println(list);
+        EventScanner eventScanner = ScannerFactory.getEventScanner();
+        list = eventScanner.toString();
+        System.out.println(list);
+        
+        PVRecord pvRecord = null;
+        pvRecord = masterPVDatabase.findRecord("counterPush");
+        assertNotNull(pvRecord);
+        PVStructure pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        Structure structure = pvStructure.getStructure();
+        PVField[] pvData = pvStructure.getPVFields();        
+        int index = structure.getFieldIndex("value");
+        PVField counterPushValue = pvData[index];
+        pvRecord = masterPVDatabase.findRecord("doubleReceive09");
+        assertNotNull(pvRecord);
+        pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        structure = pvStructure.getStructure();
+        pvData = pvStructure.getPVFields();        
+        index = structure.getFieldIndex("value");
+        PVField doubleReceive09Value = pvData[index];
+        pvRecord = masterPVDatabase.findRecord("counterEvent0");
+        assertNotNull(pvRecord);
+        pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        structure = pvStructure.getStructure();
+        pvData = pvStructure.getPVFields();       
+        index = structure.getFieldIndex("value");
+        PVField counterEvent0Value = pvData[index];
+        pvRecord = masterPVDatabase.findRecord("counterEvent1");
+        pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        assertNotNull(pvRecord);
+        structure = pvStructure.getStructure();
+        pvData = pvStructure.getPVFields();           
+        index = structure.getFieldIndex("value");
+        PVField counterEvent1Value = pvData[index];
+        for(int i=0; i<5; i++) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("    counterPush " + counterPushValue.toString());
+                System.out.println("doubleReceive09 " + doubleReceive09Value.toString());
+                System.out.println("  counterEvent0 " + counterEvent0Value.toString());
+                System.out.println("  counterEvent1 " + counterEvent1Value.toString());
+                System.out.println();
+            } catch (InterruptedException e) {
+            }
+        }
+        ok = install.installRecords("test/org/epics/ioc/support/scanAddPV.xml", iocRequester);
+        if(!ok) return;
+       
+        String[] recordList = masterPVDatabase.recordList(".*");
+        System.out.print("record list");
+        for(int i=0; i<recordList.length; i++) {
+            if((i+1)%5 == 0) {
+                System.out.println();
+                System.out.print("    ");
+            } else {
+                System.out.print(" ");
+            }
+            System.out.print(recordList[i]);
+        }
+        System.out.println();
+        pvRecord = masterPVDatabase.findRecord("counter");
+        assertNotNull(pvRecord);
+        pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        structure = pvStructure.getStructure();
+        pvData = pvStructure.getPVFields();            
+        index = structure.getFieldIndex("value");
+        PVField counterValue = pvData[index];
+        pvRecord = masterPVDatabase.findRecord("double02");
+        assertNotNull(pvRecord);
+        pvStructure = pvRecord.getPVRecordStructure().getPVStructure();
+        structure = pvStructure.getStructure();
+        pvData = pvStructure.getPVFields();
+        index = structure.getFieldIndex("value");
+        PVField double02Value = pvData[index];
+        list = periodicScanner.toString();
+        System.out.println(list);
+        int counter = 0;
+        while(true) {
+        	counter++;
+        	if(counter>10) break;
+            try {
+                Thread.sleep(1000);
+                System.out.println("    counterPush " + counterPushValue.toString());
+                System.out.println("doubleReceive09 " + doubleReceive09Value.toString());
+                System.out.println("  counterEvent0 " + counterEvent0Value.toString());               
+                System.out.println("  counterEvent1 " + counterEvent1Value.toString());
+                System.out.println(" counter " + counterValue.toString());
+                System.out.println("double02 " + double02Value.toString());
+                System.out.println();
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+    
+    private static class RequesterForTesting implements Requester {
+        private String requesterName = null;
+        
+        RequesterForTesting(String requesterName) {
+            this.requesterName = requesterName;
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#getRequestorName()
+         */
+        public String getRequesterName() {
+            return requesterName;
+        }
+
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+         */
+        public void message(String message, MessageType messageType) {
+            System.out.println(message);
+            if(messageType.ordinal()>maxMessageType.ordinal()) maxMessageType = messageType;
+        }
+    }
+
+}
---
+++ test/org/epics/ioc/support/SimpleProcessTest.java
@@ -0,0 +1,215 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this disctibution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.ioc.support;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import junit.framework.TestCase;
+
+import org.epics.ioc.database.PVDatabase;
+import org.epics.ioc.database.PVDatabaseFactory;
+import org.epics.ioc.database.PVRecord;
+import org.epics.ioc.install.Install;
+import org.epics.ioc.install.InstallFactory;
+import org.epics.ioc.support.ProcessToken;
+import org.epics.ioc.support.RecordProcess;
+import org.epics.ioc.support.RecordProcessRequester;
+import org.epics.ioc.util.RequestResult;
+import org.epics.ioc.xml.XMLToPVDatabaseFactory;
+import org.epics.pvData.property.TimeStamp;
+import org.epics.pvData.property.TimeStampFactory;
+import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.Requester;
+
+
+/**
+ * JUnit test for RecordProcess.
+ * @author mrk
+ *
+ */
+public class SimpleProcessTest extends TestCase {
+    private static final PVDatabase masterPVDatabase = PVDatabaseFactory.getMaster();
+    private static final Install install = InstallFactory.get();
+    private static MessageType maxMessageType = MessageType.info;
+    /**
+     * test PVAccess.
+     */
+    public static void testProcess() {
+        Requester iocRequester = new RequesterForTesting("simpleProcessTest");
+        XMLToPVDatabaseFactory.convert(masterPVDatabase,"${JAVAIOC}/xml/structures.xml", iocRequester,false,null,null,null);
+        if(maxMessageType!=MessageType.info&&maxMessageType!=MessageType.warning) return;
+        boolean ok = install.installRecords("test/org/epics/ioc/support/simpleTestPV.xml", iocRequester);
+        PVRecord[] pvRecords;
+        if(!ok) {
+            System.out.printf("\nrecords\n");
+            pvRecords = masterPVDatabase.getRecords();
+            for(PVRecord pvRecord: pvRecords) {
+                System.out.print(pvRecord.toString());
+            }
+            return;
+        }
+        try {
+            Thread.sleep(1000);
+            System.out.println();
+        } catch (InterruptedException e) {}
+        pvRecords = masterPVDatabase.getRecords();
+        PVRecord pvRecord = masterPVDatabase.findRecord("simple");
+        assertNotNull(pvRecord);
+        Requester pvDatabaseRequester = new PVDatabaseRequester();
+        masterPVDatabase.addRequester(pvDatabaseRequester);
+        TestProcess testProcess = new TestProcess(pvRecord);
+        for(PVRecord record: pvRecords) {
+            RecordProcess recordProcess = pvRecord.getRecordProcess();
+            recordProcess.setTrace(true);
+        }
+        testProcess.test(); 
+    }
+    
+    
+    private static class RequesterForTesting implements Requester {
+        private String requesterName = null;
+        
+        RequesterForTesting(String requesterName) {
+            this.requesterName = requesterName;
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#getRequestorName()
+         */
+        public String getRequesterName() {
+            return requesterName;
+        }
+
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+         */
+        public void message(String message, MessageType messageType) {
+            System.out.println(message);
+            if(messageType.ordinal()>maxMessageType.ordinal()) maxMessageType = messageType;
+        }
+    }
+    
+    private static class PVDatabaseRequester implements Requester {
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#getRequestorName()
+         */
+        public String getRequesterName() {
+            return "ProcessTest";
+        }
+
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+         */
+        public void message(String message, MessageType messageType) {
+            System.out.println("ProcessTest: " + messageType + " " + message);
+            
+        }
+    }
+    
+    private static class TestProcess implements RecordProcessRequester {
+    	private ProcessToken processToken = null;
+        private RecordProcess recordProcess = null;
+        private Lock lock = new ReentrantLock();
+        private Condition waitDone = lock.newCondition();
+        private boolean allDone = false;
+        private TimeStamp timeStamp = TimeStampFactory.create();
+        private long startTime = 0;
+        
+        private TestProcess(PVRecord pvRecord) {
+            recordProcess = pvRecord.getRecordProcess();
+            assertNotNull(recordProcess);
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+         */
+        public void message(String message, MessageType messageType) {
+            System.out.println(message + " messageType " + messageType.toString());
+        }
+
+        void test() {
+            allDone = false;
+            timeStamp.put(System.currentTimeMillis());
+            processToken = recordProcess.requestProcessToken(this);
+            if(processToken==null) {
+                System.out.println("could not become recordProcessor");
+                return;
+            }
+            startTime = System.nanoTime();
+            recordProcess.queueProcessRequest(processToken);
+            if(!allDone) {
+                lock.lock();
+                try {
+                    while(!allDone) {
+                        waitDone.await();
+                    }
+                } catch (InterruptedException ie) {
+                    return;
+                } finally {
+                    lock.unlock();
+                }
+            }
+            recordProcess.releaseProcessToken(processToken);
+        }
+        
+		void showTime(String message) {
+			long endTime = System.nanoTime();
+			double diff = endTime - startTime;
+			diff /= 1e9;
+			System.out.println(message + " time " + diff + " seconds");
+		}
+        /* (non-Javadoc)
+         * @see org.epics.ioc.process.RecordProcessRequester#getRecordProcessRequestorName()
+         */
+        public String getRequesterName() {
+            return "testProcess";
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.support.RecordProcessRequester#becomeProcessor()
+         */
+        @Override
+		public void becomeProcessor() {
+        	showTime("becomeProcessor");
+        	recordProcess.process(processToken,false,timeStamp);
+		}
+		/* (non-Javadoc)
+		 * @see org.epics.ioc.support.RecordProcessRequester#canNotProcess(java.lang.String)
+		 */
+		@Override
+		public void canNotProcess(String reason) {
+			showTime("canNotProcess "+ reason);
+			 throw new IllegalStateException("canNotProcess");
+		}
+		/* (non-Javadoc)
+		 * @see org.epics.ioc.support.RecordProcessRequester#lostRightToProcess()
+		 */
+		@Override
+		public void lostRightToProcess() {
+			throw new IllegalStateException("lostRightToProcess");
+		}
+        /* (non-Javadoc)
+         * @see org.epics.ioc.process.RecordProcessRequester#processComplete(org.epics.ioc.process.Support, org.epics.ioc.process.ProcessResult)
+         */
+		@Override
+        public void recordProcessComplete() {
+			showTime("recordProcessComplete");
+            lock.lock();
+            try {
+                allDone = true;
+                    waitDone.signal();
+            } finally {
+            	lock.unlock();
+            }
+        }
+        /* (non-Javadoc)
+         * @see org.epics.ioc.process.RecordProcessRequester#requestResult(org.epics.ioc.util.AlarmSeverity, java.lang.String, org.epics.ioc.util.TimeStamp)
+         */
+		@Override
+        public void recordProcessResult(RequestResult requestResult) {
+			showTime("recordProcessResult " + requestResult);
+        }
+    }
+}
